9afbf17cdcdedbaf9fb6d477672ba107164fd804
Akshay <akshaylavan783@gmail.com>
2025-02-11 18:52:42 +0530

    add Banner and Footer components; update Home and App to include them

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import MyAppointments from './pages/MyAppointments'
 import Appointment from './pages/Appointment'
 import Navbar from './components/Navbar'
 import Contact from './pages/Contact'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -26,6 +27,7 @@ const App = () => {
         <Route path='/my-appointments' element={<MyAppointments />} />
         <Route path='/appointment/:docId' element={<Appointment />} />
       </Routes>
+      <Footer/>
     </div>
     </>
   )
